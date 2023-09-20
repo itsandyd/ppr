9138fe30ca4479cfe776732a036a02fcaa0d30a5
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-20 04:06:05 -0500

    try again

--- app/(store)/(sounds)/sounds/page.tsx
+++ app/(store)/(sounds)/sounds/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/store/store/footer";
 import Navbar from "@/components/store/store/navbar";
 
-export const SoundsPage = () => {
+const SoundsPage = () => {
     return ( 
         <div>
             <Navbar />
