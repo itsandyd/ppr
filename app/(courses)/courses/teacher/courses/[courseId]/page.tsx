5487b3f53aed6034bb92de83e0c188a04bbcf3db
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-24 03:26:41 -0500

    building course create form and api route

--- app/(courses)/courses/teacher/courses/[courseId]/page.tsx
+++ app/(courses)/courses/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,71 @@
-const CourseIdPage = () => {
+import { IconBadge } from "@/components/courses/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import TitleForm from "./components/title-form";
+
+const CourseIdPage = async ({
+    params,
+}: {
+    params: {
+        courseId: string;
+    };
+}) => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId,
+        },
+    });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.courseCategoryId
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`
+
     return ( 
-        <div>
-            <h1>Course ID Page</h1>
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course Setup
+                    </h1>
+                <span className="text-sm text-slate-700">
+                        Complete all the fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard} variant="success" />
+                        <h2 className="text-xl font-bold">Customize your course</h2>
+                    </div>
+                    <TitleForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                </div>
+            </div>
         </div>
      );
 }
