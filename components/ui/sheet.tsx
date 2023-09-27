4e97c9e16961bdf273057d5e5b89bf203fadbaa0
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-27 03:01:18 -0500

    update sheet

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -13,11 +13,10 @@ const SheetTrigger = SheetPrimitive.Trigger
 
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = ({
-  className,
-  ...props
-}: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
+interface ExtendedDialogPortalProps extends SheetPrimitive.DialogPortalProps {}
+
+const SheetPortal = ({ ...props }: ExtendedDialogPortalProps) => (
+  <SheetPrimitive.Portal {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
