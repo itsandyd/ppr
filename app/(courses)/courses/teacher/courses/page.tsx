fd0ff9be97ffa74c1126ce16ec5f61ba655fc2b6
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-28 04:09:56 -0500

    building teacher page table

--- app/(courses)/courses/teacher/courses/page.tsx
+++ app/(courses)/courses/teacher/courses/page.tsx
@@ -1,13 +1,42 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns"; 
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
+const TeacherCoursesPage = async () => {
 
-const TeacherCoursesPage = () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    if (!courses.length) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-3xl font-bold text-gray-900">No Courses</h1>
+                <Link href="/courses/create" className="p-6">
+                    <Button>Create Course</Button>
+                </Link>
+            </div>
+        );
+    }
+        
     return ( 
         <div className="p-6">
-            <Link href="/courses/teacher/create">
-                <Button>New Course</Button>
-            </Link>
+            <DataTable columns={columns} data={courses} />
         </div>
      );
 }
