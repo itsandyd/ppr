d04faf3506ebc085a95b83d7219fc435333eb052
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-28 11:49:01 -0500

    added column for editing in teachers course table

--- app/(courses)/courses/teacher/courses/components/columns.tsx
+++ app/(courses)/courses/teacher/courses/components/columns.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
+import Link from "next/link"
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -15,6 +17,20 @@ export const columns: ColumnDef<Course>[] = [
             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
           >
             Title
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Description
             <ArrowUpDown className="ml-2 h-4 w-4" />
           </Button>
         )
@@ -48,4 +64,29 @@ export const columns: ColumnDef<Course>[] = [
         )
       },
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+        const { id } = row.original
+
+        return (
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant='ghost'>
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <Link href={`/courses/teacher/courses/${id}`}>
+                        <DropdownMenuItem>
+                            <Pencil className="h-4 w-4 mr-2"/> Edit
+                        </DropdownMenuItem>
+                    </Link>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        )
+    }
+  }
+  
 ]
