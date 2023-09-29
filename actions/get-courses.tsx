bd8a7a68fc7caaef0070f2d39c0c5ba79b03947d
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-28 19:34:25 -0500

    updated course list and get courses

--- actions/get-courses.tsx
+++ actions/get-courses.tsx
@@ -1,78 +1,81 @@
-import { Course, CourseCategory } from "@prisma/client"
+import { CourseCategory, Course } from "@prisma/client";
 
-import { getProgress } from "./get-progress"
+import { getProgress } from "@/actions/get-progress";
 import { db } from "@/lib/db";
 
-type CoursesWithProgressWithCategory = Course & {
-    category?: CourseCategory | null,
-    chapters?: { id: string}[],
-    progress?: number | null,
-};
+type CourseWithProgressWithCategory = Course & {
+    courseCategory: CourseCategory | null;
+    courseChapters: { id: string }[];
+    progress: number | null;
+  };
 
 type GetCourses = {
-    userId: string,
-    title?: string,
-    categoryId?: string,
-}
+  userId: string;
+  title?: string;
+  courseCategoryId?: string;
+};
 
 export const getCourses = async ({
-    userId,
-    title,
-    categoryId,
-}: GetCourses): Promise<CoursesWithProgressWithCategory[]> => {
-    try {
-        const courses = await db.course.findMany({
-            where: {
-                isPublished: true,
-                title: {
-                    contains: title || "",
-                },
-                courseCategoryId: categoryId || undefined,
-            },
-            include: {
-                CourseCategory: true,
-                CourseChapter: {
-                    where: {
-                        isPublished: true,
-                    },
-                    select: {
-                        id: true,
-                    },
-                },
-                PurchaseCourse: {
-                    where: {
-                        userId,
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: "desc",
+  userId,
+  title,
+  courseCategoryId
+}: GetCourses): Promise<CourseWithProgressWithCategory[]> => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        isPublished: true,
+        title: {
+          contains: title,
+        },
+        courseCategoryId
+      },
+      include: {
+        CourseCategory: true,
+        CourseChapter: {
+          where: {
+            isPublished: true,
+          },
+          select: {
+            id: true,
+          }
+        },
+        PurchaseCourse: {
+          where: {
+            userId,
+          }
+        }
+      },
+      orderBy: {
+        createdAt: "desc",
+      }
+    });
+
+    const coursesWithProgress: CourseWithProgressWithCategory[] = await Promise.all(
+        courses.map(async course => {
+          const courseCategory = course.CourseCategory || { id: '', name: '' };
+          if (course.PurchaseCourse.length === 0) {
+            return {
+              ...course,
+              courseCategory,
+              courseChapters: course.CourseChapter,
+              progress: null,
             }
+          }
+      
+          const progressPercentage = await getProgress(userId, course.id);
+      
+          return {
+            ...course,
+            courseCategory,
+            courseChapters: course.CourseChapter,
+            progress: progressPercentage,
+          };
         })
+      );
 
-        const coursesWithProgress: CoursesWithProgressWithCategory[] = await Promise.all(
-            courses.map(async (course) => {
-                if (course.PurchaseCourse.length === 0) {
-                    return {
-                        ...course,
-                        progress: null,
-                    }
-                }
-
-                const progressPercentage = await getProgress(userId, course.id);
-
-                return {
-                    ...course,
-                    progress: progressPercentage,
-                }
-            }));
-
-            return coursesWithProgress;
-
-    } catch (error) {
-        console.log("[GET_COURSES]", error)
-        return []
-    }
-
-
+    return coursesWithProgress;
+  } catch (error) {
+    console.log("[GET_COURSES]", error);
+    return [];
+  }
 }
--- components/courses/courses-list.tsx
+++ components/courses/courses-list.tsx
@@ -3,39 +3,39 @@ import { CourseCategory, Course } from "@prisma/client";
 import { CourseCard } from "./course-card";
 
 type CourseWithProgressWithCategory = Course & {
-  category: CourseCategory | null;
-  chapters: { id: string }[];
-  progress: number | null;
-};
+    courseCategory: CourseCategory | null;
+    courseChapters: { id: string }[];
+    progress: number | null;
+  };
 
 interface CoursesListProps {
   items: CourseWithProgressWithCategory[];
 }
 
 export const CoursesList = ({
-  items
-}: CoursesListProps) => {
-  return (
-    <div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
-        {items.map((item) => (
-          <CourseCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            imageUrl={item.imageUrl!}
-            chaptersLength={item.chapters.length}
-            price={item.price!}
-            progress={item.progress}
-            category={item?.category?.name!}
-          />
-        ))}
-      </div>
-      {items.length === 0 && (
-        <div className="text-center text-sm text-muted-foreground mt-10">
-          No courses found
+    items
+  }: CoursesListProps) => {
+    return (
+      <div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+          {items.map((item) => (
+            <CourseCard
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              imageUrl={item.imageUrl!}
+              chaptersLength={item.courseChapters.length}
+              price={item.price!}
+              progress={item.progress}
+              category={item?.courseCategory?.name!}
+            />
+          ))}
         </div>
-      )}
-    </div>
-  )
-}
+        {items.length === 0 && (
+          <div className="text-center text-sm text-muted-foreground mt-10">
+            No courses found
+          </div>
+        )}
+      </div>
+    )
+  }
