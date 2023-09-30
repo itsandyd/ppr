c98e386695a2e7cc97334265fa025b5fe6b46fd3
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-30 07:03:14 -0500

    video url no longer required

--- app/(academy)/academy/dashboard/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(academy)/academy/dashboard/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -45,7 +45,7 @@ const ChapterIdPage = async ({
     const requiredFields = [
         chapter.title,
         chapter.description,
-        chapter.videoUrl,
+        // chapter.videoUrl,
     ];
 
     const totalFields = requiredFields.length;
