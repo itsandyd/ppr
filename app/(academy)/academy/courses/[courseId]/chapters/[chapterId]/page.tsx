e7a94827d8033826e009882beec07f536a4d9493
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-29 22:25:41 -0500

    one more deploy

--- app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,42 @@
 import { CourseCard } from '@/components/courses/course-card';
 import { CourseNavbar } from '@/components/courses/navbar';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import react from 'react';
 
-export const ChapterIdPage = () => {
+const ChapterIdPage = async ({
+    params
+  }: {
+    params: { courseId: string; chapterId: string }
+  }) => {
+    const { userId } = auth();
+    
+    if (!userId) {
+      return redirect("/");
+    } 
+
+    // const {
+    //     chapter,
+    //     course,
+    //     muxData,
+    //     attachments,
+    //     nextChapter,
+    //     userProgress,
+    //     purchase,
+    //   } = await getChapter({
+    //     userId,
+    //     chapterId: params.chapterId,
+    //     courseId: params.courseId,
+    //   });
+    
+    //   if (!chapter || !course) {
+    //     return redirect("/")
+    //   }
+
+
     return ( 
         <div>
             <h1>Chapter Id Page</h1>
-            <CourseNavbar />
         </div>
      );
 }
