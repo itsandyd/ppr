71988d26890bcccef81bf0f61fb89a93cc873da4
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-29 22:10:27 -0500

    updating the app

--- app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,3 +1,5 @@
+import react from 'react';
+
 export const ChapterIdPage = () => {
     return ( 
         <div>
--- app/(academy)/academy/courses/[courseId]/components/course-navbar.tsx
+++ app/(academy)/academy/courses/[courseId]/components/course-navbar.tsx
@@ -16,6 +16,10 @@ export const CourseNavbar = ({
 }: CourseSidebarProps) => {
     return ( 
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+            {/* <CourseMobileSidebar 
+                course={course}
+                progressCount={progressCount}
+            /> */}
             <CourseNavbarRoutes />
         </div>
      );
