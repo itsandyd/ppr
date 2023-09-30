df4162be765d93010483a0223041bbc6017463d2
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-29 22:18:03 -0500

    trying to deploy again

--- app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(academy)/academy/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,12 @@
+import { CourseCard } from '@/components/courses/course-card';
+import { CourseNavbar } from '@/components/courses/navbar';
 import react from 'react';
 
 export const ChapterIdPage = () => {
     return ( 
         <div>
             <h1>Chapter Id Page</h1>
+            <CourseNavbar />
         </div>
      );
 }
