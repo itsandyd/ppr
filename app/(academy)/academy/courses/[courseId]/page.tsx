8cc1c5428126c77984521c29f2e5b5ac44032321
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-29 22:29:42 -0500

    created course id page

--- app/(academy)/academy/courses/[courseId]/page.tsx
+++ app/(academy)/academy/courses/[courseId]/page.tsx
@@ -1,4 +1,30 @@
-export const CourseIdPage = () => {
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
+    params
+  }: {
+    params: { courseId: string; }
+  }) => {
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+      },
+      include: {
+        courseChapter: {
+          where: {
+            isPublished: true,
+          },
+          orderBy: {
+            position: "asc"
+          }
+        }
+      }
+    });
+  
+    if (!course) {
+      return redirect("/");
+    }
     return ( 
         <div>
             Course ID Page
