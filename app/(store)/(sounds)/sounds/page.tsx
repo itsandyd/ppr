cc46272434e822c720a054036f11877d78a50045
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-10-02 00:10:53 -0500

    updated sounds page

--- app/(store)/(sounds)/sounds/page.tsx
+++ app/(store)/(sounds)/sounds/page.tsx
@@ -4,19 +4,38 @@ import Footer from "@/components/store/store/footer";
 import Navbar from "@/components/store/store/navbar";
 import Container from "@/components/store/store/ui/container";
 import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-export const revalidate = 0;
+interface SoundsPageProps {
+    params: {
+      storeId: string;
+    }
+  }
 
-const SoundsPage = async () => {
+const SoundsPage = async ({
+    params,
+}: SoundsPageProps) => {
+    const store = await db.store.findFirst({
+    });
+
+    if (!store) {
+        redirect("/");
+    }
+
+    // const sounds = await db.
 
     // const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
 
     return ( 
-<>
+        <>
             <div className="space-y-10 pb-10">
                 <Navbar />
-                {/* <Billboard data={billboard}/> */}
-            </div>
+                <div className="flex-col">
+                    <div className="flex-1 space-y-4 p-8 pt-6">
+                    {store.name}
+                    </div>
+                    </div>
+                </div>
       </>
      );
 }
