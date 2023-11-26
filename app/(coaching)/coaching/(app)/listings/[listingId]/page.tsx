1ea7acccbc2e86790f590fc761ad57bc6bc9dd9d
pauseplayrepeat <mrsqueex@gmail.com>
2023-11-26 07:07:29 -0600

    trying to push

--- app/(coaching)/coaching/(app)/listings/[listingId]/page.tsx
+++ app/(coaching)/coaching/(app)/listings/[listingId]/page.tsx
@@ -6,6 +6,7 @@ import EmptyState from "@/components/coaching/EmptyState";
 import getListingById from "@/actions/getListingById";
 import getReservations from "@/actions/getReservations";
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import getListings from "@/actions/getListings";
 
 interface IParams {
   listingId?: string;
@@ -13,26 +14,32 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
 
-  const listing = await getListingById(params);
+  // const listingData = await getListingById(); // Fetch the listing data from your API or database
+
+// const listing = {
+//   ...listingData,
+//   userId: String(listingData.userId), // Convert the userId to a string
+// };
   const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
-  if (!listing) {
-    return (
-      <ClientOnly>
-        <EmptyState />
-      </ClientOnly>
-    );
-  }
+  // if (!listing) {
+  //   return (
+  //     <ClientOnly>
+  //       <EmptyState />
+  //     </ClientOnly>
+  //   );
+  // }
 
   return (
-    <ClientOnly>
-      <ListingClient
-        listing={listing}
-        reservations={reservations}
-        // currentUser={currentUser}
-      />
-    </ClientOnly>
+    // <ClientOnly>
+    //   <ListingClient
+    //     listing={listing}
+    //     reservations={reservations}
+    //     // currentUser={currentUser}
+    //   />
+    // </ClientOnly>
+    <div>Hello</div>
   );
 }
  
