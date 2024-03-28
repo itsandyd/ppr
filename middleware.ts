5d43fac24a010be9e6e51dce48c2bccd3de223bf
pauseplayrepeat <mrsqueex@gmail.com>
2024-03-28 01:26:34 -0500

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -19,34 +19,34 @@ export default authMiddleware({
     }`
 
     //if subdomain exists
-    const customSubDomain = hostname
-      .get('host')
-      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
-      .filter(Boolean)[0]
+    // const customSubDomain = hostname
+    //   .get('host')
+    //   ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    //   .filter(Boolean)[0]
 
-    if (customSubDomain) {
-      return NextResponse.rewrite(
-        new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
-      )
-    }
+    // if (customSubDomain) {
+    //   return NextResponse.rewrite(
+    //     new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    //   )
+    // }
 
-    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
-      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
-    }
+    // if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+    //   return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+    // }
 
-    if (
-      url.pathname === '/' ||
-      (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
-    ) {
-      return NextResponse.rewrite(new URL('/site', req.url))
-    }
+    // if (
+    //   url.pathname === '/' ||
+    //   (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    // ) {
+    //   return NextResponse.rewrite(new URL('/', req.url))
+    // }
 
-    if (
-      url.pathname.startsWith('/agency') ||
-      url.pathname.startsWith('/subaccount')
-    ) {
-      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
-    }
+    // if (
+    //   url.pathname.startsWith('/agency') ||
+    //   url.pathname.startsWith('/subaccount')
+    // ) {
+    //   return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+    // }
   },
 });
  
