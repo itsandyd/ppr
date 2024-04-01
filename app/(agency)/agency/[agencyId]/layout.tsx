43eda8ea01c39d7f57144b2ed5b7c47ae7635ffe
pauseplayrepeat <mrsqueex@gmail.com>
2024-03-31 21:10:46 -0500

    trying to push

---
+++ app/(agency)/agency/[agencyId]/layout.tsx
@@ -0,0 +1,56 @@
+
+import Sidebar from '@/components/agency/sidebar'
+import { getNotificationAndUser, verifyAndAcceptInvitation } from '@/lib/agency/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode
+  params: { agencyId: string }
+}
+
+const layout = async ({ children, params }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation()
+  const user = await currentUser()
+
+  if (!user) {
+    return redirect('/')
+  }
+
+  if (!agencyId) {
+    return redirect('/agency')
+  }
+
+//   if (
+//     user.privateMetadata.role !== 'AGENCY_OWNER' &&
+//     user.privateMetadata.role !== 'AGENCY_ADMIN'
+//   )
+//     return <Unauthorized />
+
+  let allNoti: any = []
+  const notifications = await getNotificationAndUser(agencyId)
+  if (notifications) allNoti = notifications
+
+  return (
+    <div className="h-screen">
+      <Sidebar
+        id={params.agencyId}
+        type="agency"
+      />
+      <div className="md:pl-[300px]">
+        {/* <InfoBar
+          notifications={allNoti}
+          role={allNoti.User?.role}
+        /> */}
+        <div className="relative">
+          {/* <BlurPage> */}
+            {children}
+            {/* </BlurPage> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default layout
---
+++ lib/agency/constants.ts
@@ -0,0 +1,153 @@
+import { BarChart, Calendar, CheckCircle, Compass, ComputerIcon, Database, Flag, Headphones, Home, Info, LinkIcon, MessageCircle, Music2, PersonStanding, Power, Receipt, Send, Settings, Shield, Star, Video, Wallet } from "lucide-react";
+import { BsPersonBadge } from "react-icons/bs";
+import { FaRoad } from "react-icons/fa";
+import { IoWarning } from "react-icons/io5";
+import { MdPayment } from "react-icons/md";
+
+export const icons = [
+    {
+      value: 'chart',
+      label: 'Bar Chart',
+      path: BarChart,
+    },
+    {
+      value: 'headphone',
+      label: 'Headphones',
+      path: Headphones,
+    },
+    {
+      value: 'send',
+      label: 'Send',
+      path: Send,
+    },
+    {
+      value: 'pipelines',
+      label: 'Pipelines',
+      path: FaRoad,
+    },
+    {
+      value: 'calendar',
+      label: 'Calendar',
+      path: Calendar,
+    },
+    {
+      value: 'settings',
+      label: 'Settings',
+      path: Settings,
+    },
+    {
+      value: 'check',
+      label: 'Check Circled',
+      path: CheckCircle,
+    },
+    {
+      value: 'chip',
+      label: 'Chip',
+      path: ComputerIcon,
+    },
+    {
+      value: 'compass',
+      label: 'Compass',
+      path: Compass,
+    },
+    {
+      value: 'database',
+      label: 'Database',
+      path: Database,
+    },
+    {
+      value: 'flag',
+      label: 'Flag',
+      path: Flag,
+    },
+    {
+      value: 'home',
+      label: 'Home',
+      path: Home,
+    },
+    {
+      value: 'info',
+      label: 'Info',
+      path: Info,
+    },
+    {
+      value: 'link',
+      label: 'Link',
+      path: LinkIcon,
+    },
+    {
+      value: 'lock',
+      label: 'Lock',
+      path: Lock,
+    },
+    {
+      value: 'messages',
+      label: 'Messages',
+      path: MessageCircle,
+    },
+    {
+      value: 'notification',
+      label: 'Notification',
+      path: Notification,
+    },
+    {
+      value: 'payment',
+      label: 'Payment',
+      path: MdPayment,
+    },
+    {
+      value: 'power',
+      label: 'Power',
+      path: Power,
+    },
+    {
+      value: 'receipt',
+      label: 'Receipt',
+      path: Receipt,
+    },
+    {
+      value: 'shield',
+      label: 'Shield',
+      path: Shield,
+    },
+    {
+      value: 'star',
+      label: 'Star',
+      path: Star,
+    },
+    {
+      value: 'tune',
+      label: 'Tune',
+      path: Music2,
+    },
+    {
+      value: 'videorecorder',
+      label: 'Video Recorder',
+      path: Video,
+    },
+    {
+      value: 'wallet',
+      label: 'Wallet',
+      path: Wallet,
+    },
+    {
+      value: 'warning',
+      label: 'Warning',
+      path: IoWarning,
+    },
+    {
+      value: 'person',
+      label: 'Person',
+      path: PersonStanding,
+    },
+    {
+      value: 'category',
+      label: 'Category',
+      path: BsPersonBadge,
+    },
+    {
+      value: 'clipboardIcon',
+      label: 'Clipboard Icon',
+      path: Clipboard,
+    },
+  ]
