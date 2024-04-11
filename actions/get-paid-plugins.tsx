59dd95fea748af6511f87f35aad4f55bb5114ca6
pauseplayrepeat <mrsqueex@gmail.com>
2024-04-11 04:35:39 -0500

    pushing v2

--- actions/get-paid-plugins.tsx
+++ actions/get-paid-plugins.tsx
@@ -1,23 +1,25 @@
-import { Plugin, PluginCategory } from "@prisma/client";
+import { Plugin, PluginType } from "@prisma/client";
 import { db } from "@/lib/db";
 
-type PluginWithCategory = Plugin & {
-  category: PluginCategory | null;
+type PluginWithType = Plugin & {
+  pluginType: PluginType | null; // Extend Plugin with type relation
 };
 
 interface GetPaidPluginsParams {
   categoryId?: string; // Optional parameter for filtering by category
+  pluginTypeId?: string; // Optional parameter for filtering by pluginType's typeId
 }
 
-export const getPaidPlugins = async ({ categoryId }: GetPaidPluginsParams = {}): Promise<PluginWithCategory[]> => {
+export const getPaidPlugins = async ({ categoryId, pluginTypeId }: GetPaidPluginsParams = {}): Promise<PluginWithType[]> => {
   try {
-    const plugins: PluginWithCategory[] = await db.plugin.findMany({
+    const plugins: PluginWithType[] = await db.plugin.findMany({
       where: {
         pricingType: "PAID",
         ...(categoryId && { categoryId: categoryId }), // Conditionally add categoryId to the query if it exists
+        ...(pluginTypeId && { pluginTypeId: pluginTypeId }), // Conditionally add pluginTypeId to the query if it exists
       },
       include: {
-        category: true,
+        pluginType: true, // Include the pluginType relation
       },
       orderBy: {
         createdAt: "asc",
