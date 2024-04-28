33bb041673c6bc3ab57875a719e91d52bea02e96
pauseplayrepeat <mrsqueex@gmail.com>
2024-04-28 13:22:05 -0500

    updated companion navbar to go to ai home

--- components/ai/companion/companion-navbar.tsx
+++ components/ai/companion/companion-navbar.tsx
@@ -32,7 +32,7 @@ export const CompanionNavbar = ({
         <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
                 <CompanionMobileSidebar isPro={isPro}/>
-                <Link href="/ai/dashboard">
+                <Link href="/ai">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
                         companion
                     </h1>
