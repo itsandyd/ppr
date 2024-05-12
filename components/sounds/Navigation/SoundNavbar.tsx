14659d3ffdbbda7a516bbc8a1ecf670789fff0fa
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-11 21:05:40 -0500

    updated navbar backgrounds

--- components/sounds/Navigation/SoundNavbar.tsx
+++ components/sounds/Navigation/SoundNavbar.tsx
@@ -5,7 +5,7 @@ import { SoundNavbarRoutes } from "./SoundNavbarRoutes";
 export const SoundNavbar = () => {
     return ( 
         // border-b 
-        <div className="p-4 border-b h-full flex items-center shadow-sm ">
+        <div className="p-4 border-b h-full flex items-center shadow-sm bg-white dark:bg-[#313338]">
             {/* <CourseMobileSidebar /> */}
             <SoundNavbarRoutes />  
         </div>
